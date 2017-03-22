43f71b0c238847d58f5954b3f656a9c571931f33
Georg Gotschier <g.gotschier@student.tugraz.at>
2017-03-22 10:19:03 +0100

    Fixed toolbar

--- app/src/main/java/org/catroid/catrobat/newui/recycleviewlist/RecycleViewActivity.java
+++ app/src/main/java/org/catroid/catrobat/newui/recycleviewlist/RecycleViewActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import org.catroid.catrobat.newui.R;
@@ -26,6 +28,14 @@ public class RecycleViewActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_recycle_view, menu);
+        return true;
     }
 
 }
