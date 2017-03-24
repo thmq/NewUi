f52a96b1b562a05d9b3ba96bfe6176ec63d4c821
Thomas Schranz <thomas.schranz@student.tugraz.at>
2017-03-24 14:56:28 +0100

    Extended StorageHandler for folder structure setup

--- app/src/main/java/org/catroid/catrobat/newui/io/StorageHandler.java
+++ app/src/main/java/org/catroid/catrobat/newui/io/StorageHandler.java
@@ -1,6 +1,8 @@
 package org.catroid.catrobat.newui.io;
 
 
+import android.os.Environment;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,29 +12,54 @@ import java.nio.channels.FileChannel;
 
 public final class StorageHandler {
 
+    public static final String ROOT = Environment.getExternalStorageDirectory().toString() + "NewUi";
+    public static final String IMAGE_FOLDER = "images";
+    public static final String SOUND_FOLDER = "sounds";
+
+    public static final FileInfo rootDirectory = new FileInfo(null, ROOT);
+
     public static FileInfo createLookFileOnSD() {
-        return new FileInfo(null, "filepath.png");
+        return new FileInfo(rootDirectory, "filepath.png");
     }
 
     public static FileInfo createSoundFileOnSD() {
-        return new FileInfo(null, "filepath.m4a");
+        return new FileInfo(rootDirectory, "filepath.m4a");
     }
 
-    public static File copyFile(String srcPath) throws IOException {
+    public static File copyFile(String srcPath) throws Exception {
         String dstPath = new File(srcPath).getParent();
         return copyFile(srcPath, dstPath);
     }
 
-    public static File copyFile(String srcPath, String dstPath) throws IOException {
+    public static File copyFile(String srcPath, String dstPath) throws Exception {
         File srcFile = new File(srcPath);
         if (!srcFile.exists()) {
             throw new FileNotFoundException("File: " + srcPath + "does not exist.");
         }
 
-        File dstFile = new File(dstPath);
+        File dstFile = getUniqueFileName(srcPath, dstPath);
         copyFile(srcFile, dstFile);
 
         return dstFile;
+    }
+
+    private static synchronized File getUniqueFileName(String originalName, String dstDirectory) throws Exception {
+        int extensionStartIndex = originalName.lastIndexOf(".");
+        String extension = originalName.substring(extensionStartIndex);
+        String fileName = originalName.substring(0, extensionStartIndex);
+
+        int appendix = 0;
+
+        while(appendix < Integer.MAX_VALUE) {
+            String dstFileName = fileName + String.valueOf(appendix) + extension;
+            File dstFile = new File(dstDirectory, dstFileName);
+            if(!dstFile.exists()) {
+                return dstFile;
+            }
+            appendix++;
+        }
+
+        throw new Exception("Could not find a unique file name.");
     }
 
     private static void copyFile(File srcFile, File dstFile) throws IOException {
@@ -55,5 +82,25 @@ public final class StorageHandler {
             throw new FileNotFoundException("File: " + srcPath + "does not exist.");
         }
         return file.delete();
+    }
+
+    public static void setupDirectoryStructure() {
+        mkDir(ROOT);
+        mkDir(ROOT, IMAGE_FOLDER);
+        mkDir(ROOT, SOUND_FOLDER);
+    }
+
+    private static void mkDir(String name) {
+        File directory = new File(name);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+    }
+
+    private static void mkDir(String name, String parentDirectory) {
+        File directory = new File(parentDirectory, name);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
     }
 }
