b06a7b1c479464df4412a9e8111b354aea61e371
Dominik Scheiber <dominik.scheiber@student.tugraz.at>
2017-04-05 14:50:56 +0200

    Added incomplete Instrument Tests for TabView

--- app/src/androidTest/java/org/catroid/catrobat/newui/TabsInstrumentedTest.java
+++ app/src/androidTest/java/org/catroid/catrobat/newui/TabsInstrumentedTest.java
@@ -3,7 +3,6 @@ package org.catroid.catrobat.newui;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.catroid.catrobat.newui.ui.RecyclerViewActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +10,6 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
