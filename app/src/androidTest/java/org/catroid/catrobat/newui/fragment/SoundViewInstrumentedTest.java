528f696b5fc212c11a290f3c0694a3571785a714
Matthias Link <matthias@link.co.at>
2017-04-05 15:14:13 +0200

    [GG,ML] Cleanup common tests

--- app/src/androidTest/java/org/catroid/catrobat/newui/fragment/SoundViewInstrumentedTest.java
+++ app/src/androidTest/java/org/catroid/catrobat/newui/fragment/SoundViewInstrumentedTest.java
@@ -1,6 +1,5 @@
 package org.catroid.catrobat.newui.fragment;
 
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
 import org.catroid.catrobat.newui.R;
@@ -9,23 +8,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.catroid.catrobat.newui.fragment.Utils.addNewItemNamed;
-import static org.catroid.catrobat.newui.fragment.Utils.checkItemNamedExists;
-import static org.catroid.catrobat.newui.fragment.Utils.checkItemNamedNotExists;
-import static org.catroid.catrobat.newui.fragment.Utils.selectItemNamed;
-
 public class SoundViewInstrumentedTest {
     @Rule
     public ActivityTestRule<SpriteActivity> activityRule = new ActivityTestRule<>(SpriteActivity.class);
 
     @Before
     public void navigateToTab() {
-        onView(ViewMatchers.withText(R.string.tab_name_sounds)).perform(click());
+        Utils.navigateToTab(R.string.tab_name_sounds);
     }
 
     @Test
