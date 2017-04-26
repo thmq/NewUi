c392bf68032dba9b40feaba77c294363a55c3e82
Branislav Stupak <branostupak@gmail.com>
2017-04-26 15:32:11 +0200

    Added HintText class

---
+++ app/src/main/java/org/catroid/catrobat/newui/ui/HintText.java
@@ -0,0 +1,38 @@
+package org.catroid.catrobat.newui.ui;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import org.catroid.catrobat.newui.R;
+
+public class HintText {
+
+    /**
+     * @param message Message to show in HintText.
+     * @param destItem HintText will be shown under this item.
+     * @param context Current activity context.
+     * @return Returns true if HintText was added.
+     */
+    public static boolean newInstance(String message, View destItem, Context context){
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.hint_text, null, false);
+        TextView textHint = (TextView) view.findViewById(R.id.text_hint);
+        textHint.setText(message);
+
+        View parent = (View) destItem.getParent();
+
+        try {
+            int index = ((ViewGroup)parent).indexOfChild(destItem);
+            ((LinearLayoutCompat)parent).addView(view, index + 1);
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+}
